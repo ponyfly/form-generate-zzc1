9299943941b64b34e2a887e031a5838fbda37150
zhangzechao <zhangzechao@shuidihuzhu.com>
2024-04-11 17:18:30 +0800

    feat(preview): add preview some feature
    1. 添加刷新功能
    2. 添加导出功能

--- src/views/index/FormDrawer.vue
+++ src/views/index/FormDrawer.vue
@@ -32,6 +32,24 @@
           <div v-show="activeTab==='css'" id="editorCss" class="tab-editor" />
         </el-col>
         <el-col :md="24" :lg="12" class="right-preview">
+          <div class="action-bar" :style="{'text-align': 'left'}">
+              <span class="bar-btn" @click="runCode">
+                <i class="el-icon-refresh" />
+                刷新
+              </span>
+            <span class="bar-btn" @click="exportFile">
+                <i class="el-icon-download" />
+                导出vue文件
+              </span>
+            <span ref="copyBtn" class="bar-btn copy-btn">
+                <i class="el-icon-document-copy" />
+                复制代码
+              </span>
+            <span class="bar-btn delete-btn" @click="$emit('update:visible', false)">
+                <i class="el-icon-circle-close" />
+                关闭
+              </span>
+          </div>
           <iframe
               v-show="isIframeLoaded"
               ref="previewPage"
@@ -56,6 +74,8 @@ import {cookCss} from "@/components/generator/css";
 import loadBeautifier from "@/utils/loadBeautifer";
 import {beautifierConf, exportDefault} from "@/utils";
 import loadMonaco from "@/utils/loadMonaco";
+import {saveAs} from "file-saver";
+import ClipboardJS from "clipboard";
 
 const editorObj = {
   html: null,
@@ -109,7 +129,8 @@ export default {
       })
     },
     onClose() {
-
+      this.isInitcode = false
+      this.isRefreshCode = false
     },
     setEditorValue(id, type, codeStr) {
       if (editorObj[type]) {
@@ -133,6 +154,7 @@ export default {
     runCode() {
       const jsCodeStr = editorObj.js.getValue()
       try {
+        // 判断是否是export default的对象
         const ast = parse(jsCodeStr, { sourceType: 'module' })
         const astBody = ast.program.body
         if (astBody.length > 1) {
@@ -165,6 +187,38 @@ export default {
         console.error(err)
       }
     },
+    exportFile() {
+      this.$prompt('文件名:', '导出文件', {
+        inputValue: `${+new Date()}.vue`,
+        closeOnClickModal: false,
+        inputPlaceholder: '请输入文件名'
+      }).then(({ value }) => {
+        if (!value) value = `${+new Date()}.vue`
+        const codeStr = this.generateCode()
+        const blob = new Blob([codeStr], { type: 'text/plain;charset=utf-8' })
+        saveAs(blob, value)
+      })
+    },
+  },
+  mounted() {
+    window.addEventListener('keydown', this.preventDefaultSave)
+    const clipboard = new ClipboardJS('.copy-btn', {
+      text: () => {
+        const codeStr = this.generateCode()
+        this.$notify({
+          title: '成功',
+          message: '代码已复制到剪切板，可粘贴。',
+          type: 'success'
+        })
+        return codeStr
+      }
+    })
+    clipboard.on('error', () => {
+      this.$message.error('代码复制失败')
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('keydown', this.preventDefaultSave)
   }
 }
 </script>
